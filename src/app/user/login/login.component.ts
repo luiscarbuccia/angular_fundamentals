a316c43dd201352d44166f387f6b0c1b8eddca9f
Luis Carbuccia <lcarbuccia@gmail.com>
2020-08-17 17:19:36 -0400

    added initial login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -2,11 +2,13 @@ import { Component } from "@angular/core";
 import { AuthService } from '../shared/auth.service';
 import { Router } from '@angular/router';
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent{
     userName
     password
+    mouseoverLogin
 
     constructor(private authService: AuthService, private router: Router){
 
