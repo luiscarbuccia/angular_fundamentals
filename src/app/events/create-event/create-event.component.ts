d4902d4c4cd10435553e86cb6509cef926c42684
Luis Carbuccia <lcarbuccia@gmail.com>
2020-08-18 23:12:19 -0400

    creates a new event with template based form

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Router } from '@angular/router'
+import { EventService } from '../shared/event.service';
 
 @Component({
     templateUrl: './create-event.component.html'
@@ -8,12 +9,14 @@ import { Router } from '@angular/router'
 export class CreateEventComponent{
     newEvent;
     isDirty:boolean = true;
-    constructor(private router:Router){
+    constructor(private router:Router, private eventService: EventService){
 
     }
 
     saveEvent(formValues){
-        console.log(formValues);
+        this.eventService.saveEvent(formValues)
+        this.isDirty = false;
+        this.router.navigate(['/events']);
     }
 
     cancel(){
