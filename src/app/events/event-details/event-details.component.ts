19a82cf9e58c7c6dba5c4cdf830479e03a72759e
Luis Carbuccia <lcarbuccia@gmail.com>
2020-08-23 11:05:58 -0400

    filtering data

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -11,6 +11,7 @@ import { IEvent, ISession } from '../shared/event.model';
 export class EventDetailsComponent implements OnInit{
     event: IEvent;
     addMode: boolean = false;
+    filterBy: string = 'all';
     
     constructor(private eventService: EventService, private activatedRoute: ActivatedRoute){
 
